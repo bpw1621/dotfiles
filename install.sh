7f4a0b403d2cf8eb4bdaa99b8192bb8b35cbb97e
Bryan Patrick Wood <bpwood@solipsys.com>
2012-12-14 15:20:00 -0500

    Improved the way install.sh deals with symlinks

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-DOTS=( 'bashrc' 'alias_bash' 'function_bash' 'cshrc' 'inputrc' 'vimrc' 'gitconfig' )
+DOTS=( 'bashrc' 'bash_alias' 'bash_function' 'bash_profile' 'cshrc' 'inputrc' 'vimrc' 'gitconfig' )
 for DOT in ${DOTS[@]}; do
-  ln -s "dotfiles/${DOT}" "${HOME}/.${DOT}" 
+  [ -L "${HOME}/.${DOT}" ] && (unlink "${HOME}/.${DOT}")
+  [ -f "${HOME}/.${DOT}" ] && (mv "${HOME}/.${DOT}" "${HOME}/${DOT}.bak")
+  ln -s "${HOME}/.dotfiles/${DOT}" "${HOME}/.${DOT}"
 done
 
 DOTDIRS=( 'vim' 'bc' )
 for DOTDIR in ${DOTDIRS[@]}; do
-  ln -s "dotfiles/${DOTDIR}" "${HOME}/.${DOTDIR}" 
+  [ -L "${HOME}/.${DOTDIR}" ] && (unlink "${HOME}/.${DOTDIR}")
+  [ -d "${HOME}/.${DOTDIR}" ] && (mv "${HOME}/.${DOTDIR}" "${HOME}/${DOTDIR}.bak")
+  ln -s "${HOME}/.dotfiles/${DOTDIR}" "${HOME}/.${DOTDIR}"
 done
 
-ln -s .bashrc .bash_profile
